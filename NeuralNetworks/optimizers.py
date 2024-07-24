b5aa1fca3f34314fd6a4683f3152d855d2ead997
s-simsek <safaksimsek05@gmail.com>
2024-07-23 17:01:19 -0700

    updated optmizers and tests

--- NeuralNetworks/optimizers.py
+++ NeuralNetworks/optimizers.py
@@ -2,112 +2,70 @@ from abc import ABC
 
 import numpy as np
 
-from NeuralNetworks.schedulers import initialize_scheduler
 
-
-def initialize_optimizer(name,
-                         lr,
-                         lr_scheduler=None,
-                         momentum=None,
-                         clip_norm=None,
-                         lr_decay=None,
-                         staircase=None,
-                         stage_length=None):
-    
-    if name == "SGD":
-        return SGD(lr=lr,
-                   lr_scheduler=lr_scheduler,
-                   momentum=momentum,
-                   clip_norm=clip_norm,
-                   lr_decay=lr_decay,
-                   staircase=staircase,
-                   stage_length=stage_length)
-    elif name == "Adam":
-        return Adam(lr=lr,
-                    lr_scheduler=lr_scheduler,
-                    clip_norm=clip_norm,
-                    lr_decay=lr_decay,
-                    stage_length=stage_length,
-                    staircase=staircase)
-    elif name == "RMSprop":
-        return RMSprop(lr=lr,
-                       lr_scheduler=lr_scheduler,
-                       clip_norm=clip_norm,
-                       lr_decay=lr_decay,
-                       stage_length=stage_length,
-                       staircase=staircase)
-    else:
-        raise NotImplementedError
+def initialize_optimizer(name, 
+                         lr, 
+                         momentum=None, 
+                         clip_norm=None, 
+                         beta1=None, 
+                         beta2=None, 
+                         epsilon=None):
+    match name:
+        case "SGD":
+            return SGD(lr=lr, 
+                    momentum=momentum, 
+                    clip_norm=clip_norm)
+        case "Adam":
+            return Adam(lr=lr, 
+                        beta1=beta1, 
+                        beta2=beta2, 
+                        epsilon=epsilon, 
+                        clip_norm=clip_norm)
+        case "RMSprop":
+            return RMSprop(lr=lr, 
+                        beta=beta1, 
+                        epsilon=epsilon, 
+                        clip_norm=clip_norm)
+        
+        case "RMSpropNesterov":
+            return RMSpropNesterov(lr=lr, 
+                                   beta=beta1,
+                                   epsilon=epsilon, 
+                                   momentum=momentum, 
+                                   clip_norm=clip_norm)
+        case _:
+            raise NotImplementedError
+        
 
 
 class Optimizer(ABC):
     def __init__(self):
         self.lr = None
-        self.lr_scheduler = None
 
 
 class SGD(Optimizer):
-    def __init__(
-        self,
-        lr,
-        lr_scheduler,
-        momentum=0.0,
-        clip_norm=None,
-        lr_decay=0.9,
-        stage_length=None,
-        staircase=None,
-    ):
+    def __init__(self, lr, momentum=0.0, clip_norm=None):
         self.lr = lr
-        self.lr_scheduler = initialize_scheduler(
-            lr_scheduler,
-            lr=lr,
-            decay=lr_decay,
-            stage_length=stage_length,
-            staircase=staircase,
-        )
         self.momentum = momentum
         self.clip_norm = clip_norm
         self.cache = {}
 
-    def update(self, param_name, param, param_grad, epoch):
+    def update(self, param_name, param, param_grad):
         if param_name not in self.cache:
             self.cache[param_name] = np.zeros_like(param)
 
         if self.clip_norm is not None:
             if np.linalg.norm(param_grad) > self.clip_norm:
-                param_grad = (
-                    param_grad * self.clip_norm / np.linalg.norm(param_grad)
-                )
+                param_grad = param_grad * self.clip_norm / np.linalg.norm(param_grad)
 
-        lr = self.lr_scheduler(epoch)
-        delta = (
-            self.momentum * self.cache[param_name]
-            + lr * param_grad
-        )
+        delta = self.momentum * self.cache[param_name] + self.lr * param_grad
         self.cache[param_name] = delta
         return delta
-    
+
+
 class Adam(Optimizer):
-    def __init__(
-        self,
-        lr,
-        lr_scheduler,
-        beta1=0.9,
-        beta2=0.999,
-        epsilon=1e-8,
-        clip_norm=None,
-        lr_decay=0.9,
-        stage_length=None,
-        staircase=None,
-    ):
+    def __init__(self, lr, beta1=0.9, beta2=0.999, epsilon=1e-8, clip_norm=None):
         self.lr = lr
-        self.lr_scheduler = initialize_scheduler(
-            lr_scheduler,
-            lr=lr,
-            decay=lr_decay,
-            stage_length=stage_length,
-            staircase=staircase,
-        )
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
@@ -116,64 +74,71 @@ class Adam(Optimizer):
         self.v = {}
         self.t = 0
 
-    def update(self, param_name, param, param_grad, epoch):
+    def update(self, param_name, param, param_grad):
         if param_name not in self.m:
             self.m[param_name] = np.zeros_like(param)
             self.v[param_name] = np.zeros_like(param)
 
         if self.clip_norm is not None:
             if np.linalg.norm(param_grad) > self.clip_norm:
-                param_grad = (
-                    param_grad * self.clip_norm / np.linalg.norm(param_grad)
-                )
+                param_grad = param_grad * self.clip_norm / np.linalg.norm(param_grad)
 
         self.t += 1
-        lr = self.lr_scheduler(epoch)
         self.m[param_name] = self.beta1 * self.m[param_name] + (1 - self.beta1) * param_grad
         self.v[param_name] = self.beta2 * self.v[param_name] + (1 - self.beta2) * (param_grad ** 2)
 
         m_hat = self.m[param_name] / (1 - self.beta1 ** self.t)
         v_hat = self.v[param_name] / (1 - self.beta2 ** self.t)
 
-        delta = lr * m_hat / (np.sqrt(v_hat) + self.epsilon)
+        delta = self.lr * m_hat / (np.sqrt(v_hat) + self.epsilon)
         return delta
 
+
 class RMSprop(Optimizer):
-    def __init__(
-        self,
-        lr,
-        lr_scheduler,
-        beta=0.9,
-        epsilon=1e-8,
-        clip_norm=None,
-        lr_decay=0.9,
-        stage_length=None,
-        staircase=None,
-    ):
+    def __init__(self, lr, beta=0.9, epsilon=1e-8, clip_norm=None):
         self.lr = lr
-        self.lr_scheduler = initialize_scheduler(
-            lr_scheduler,
-            lr=lr,
-            decay=lr_decay,
-            stage_length=stage_length,
-            staircase=staircase,
-        )
         self.beta = beta
         self.epsilon = epsilon
         self.clip_norm = clip_norm
         self.cache = {}
 
-    def update(self, param_name, param, param_grad, epoch):
+    def update(self, param_name, param, param_grad):
         if param_name not in self.cache:
             self.cache[param_name] = np.zeros_like(param)
 
         if self.clip_norm is not None:
             if np.linalg.norm(param_grad) > self.clip_norm:
-                param_grad = (
-                    param_grad * self.clip_norm / np.linalg.norm(param_grad)
-                )
+                param_grad = param_grad * self.clip_norm / np.linalg.norm(param_grad)
 
-        lr = self.lr_scheduler(epoch)
         self.cache[param_name] = self.beta * self.cache[param_name] + (1 - self.beta) * (param_grad ** 2)
-        delta = lr * param_grad / (np.sqrt(self.cache[param_name]) + self.epsilon)
+        delta = self.lr * param_grad / (np.sqrt(self.cache[param_name]) + self.epsilon)
+        return delta
+    
+class RMSpropNesterov(Optimizer):
+    def __init__(self, lr, beta=0.9, epsilon=1e-8, momentum=0.9, clip_norm=None):
+        self.lr = lr
+        self.beta = beta
+        self.epsilon = epsilon
+        self.momentum = momentum
+        self.clip_norm = clip_norm
+        self.cache = {}
+        self.velocity = {}
+
+    def update(self, param_name, param, param_grad):
+        if param_name not in self.cache:
+            self.cache[param_name] = np.zeros_like(param)
+        if param_name not in self.velocity:
+            self.velocity[param_name] = np.zeros_like(param)
+
+        if self.clip_norm is not None:
+            if np.linalg.norm(param_grad) > self.clip_norm:
+                param_grad = param_grad * self.clip_norm / np.linalg.norm(param_grad)
+
+        self.cache[param_name] = self.beta * self.cache[param_name] + (1 - self.beta) * (param_grad ** 2)
+        
+        # Nesterov momentum
+        prev_velocity = self.velocity[param_name].copy()
+        self.velocity[param_name] = self.momentum * self.velocity[param_name] - self.lr * param_grad / (np.sqrt(self.cache[param_name]) + self.epsilon)
+        delta = self.momentum * self.momentum * prev_velocity - (1 + self.momentum) * self.velocity[param_name]
+
         return delta
